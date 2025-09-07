d955a8d6f5077de52a221f82e28541ffae10bf64
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-09-07 12:18:18 -0300

    Adiciona lógica de filtro e exibição de itens na tela inicial

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,71 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity, Text, FlatList } from "react-native";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { Item } from "@/components/Item";
+
 import { styles } from "./styles";
+import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+const ITEMS = [
+  {
+    id: "1",
+    status: FilterStatus.DONE,
+    description: "Comprar um pacote de café",
+  },
+  {
+    id: "2",
+    status: FilterStatus.PENDING,
+    description: "Comprar um pacote de arroz",
+  },
+  {
+    id: "3",
+    status: FilterStatus.DONE,
+    description: "Comprar um pacote de macarrão",
+  },
+];
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status: FilterStatus) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={ITEMS}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Item
+              data={item}
+              onStatusChange={() => console.log("Alterar status")}
+              onRemove={() => console.log("Remover")}
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => (
+            <Text style={styles.empty}>Nenhum Item Aqui</Text>
+          )}
+        />
+      </View>
     </View>
   );
 }
